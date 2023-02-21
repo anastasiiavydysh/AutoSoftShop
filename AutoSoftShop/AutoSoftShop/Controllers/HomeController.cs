0465c5f2c57703eb09f4e688b428bb259c05e779
Anastasiia Vydysh <anastasiiavydysh@gmail.com>
2023-02-22 01:43:09 +0200

    Add project files.

---
+++ AutoSoftShop/AutoSoftShop/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoSoftShop.AutoSoftShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
