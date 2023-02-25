a98731a801906b12b1399d9d116572950ad35ecd
Anastasiia Vydysh <anastasiiavydysh@gmail.com>
2023-02-26 00:32:14 +0200

    connected DB

---
+++ AutoSoftShop/AutoSoftShopContext.cs
@@ -0,0 +1,99 @@
+﻿using AutoSoftShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoSoftShop;
+
+public partial class AutoSoftShopContext : DbContext
+{
+    public AutoSoftShopContext()
+    {
+    }
+
+    public AutoSoftShopContext(DbContextOptions<AutoSoftShopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Client> Clients { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql("F:\\Course_NET\\a2159123-4ef9-4614-9ea8-35f380f740f6\\secrets.json");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Client>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("clients_pkey");
+
+            entity.ToTable("clients");
+
+            entity.HasIndex(e => e.PhoneNumber, "phone_number").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Email)
+                .HasMaxLength(30)
+                .HasColumnName("email");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(30)
+                .HasColumnName("first_name");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(30)
+                .HasColumnName("last_name");
+            entity.Property(e => e.PhoneNumber)
+                .HasMaxLength(30)
+                .HasColumnName("phone_number");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("orders_pkey");
+
+            entity.ToTable("orders");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.ClientId).HasColumnName("client_id");
+            entity.Property(e => e.ProductId).HasColumnName("product_id");
+            entity.Property(e => e.RequestTime)
+                .HasColumnType("timestamp without time zone")
+                .HasColumnName("request_time");
+
+            entity.HasOne(d => d.Client).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.ClientId)
+                .HasConstraintName("orders_client_id_fkey");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.Orders)
+                .HasForeignKey(d => d.ProductId)
+                .HasConstraintName("orders_product_id_fkey");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("products_pkey");
+
+            entity.ToTable("products");
+
+            entity.HasIndex(e => e.Name, "name").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Description)
+                .HasMaxLength(255)
+                .HasColumnName("description");
+            entity.Property(e => e.Name)
+                .HasMaxLength(30)
+                .HasColumnName("name");
+            entity.Property(e => e.Price)
+                .HasColumnType("money")
+                .HasColumnName("price");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
